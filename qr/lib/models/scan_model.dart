0f61b4fa98b1df6469bd88b4329085f4d88ee4dd
David Acevedo Jaramillo <dacevedo@glimacjv3n.local>
2020-05-09 17:05:35 -0500

    fix

--- qr/lib/models/scan_model.dart
+++ qr/lib/models/scan_model.dart
@@ -1,4 +1,6 @@
 
+
+import  'package:latlong/latlong.dart';
 class ScanModel {
   int id;
   String tipo;
@@ -25,4 +27,11 @@ class ScanModel {
       "tipo": tipo,
       "valor": valor,
   };
+
+  getLatLtn() {
+    final lalo = valor.substring(4).split(',');
+    final lat = double.parse(lalo[0]);
+    final lng = double.parse(lalo[1]);
+    return LatLng(lat, lng);
+  }
 }
--- qr/lib/pages/mapas_pages.dart
+++ qr/lib/pages/mapas_pages.dart
@@ -1,7 +1,12 @@
+
 import 'package:flutter/material.dart';
+import 'package:flutter_map/plugin_api.dart';
 import 'package:qr/models/scan_model.dart';
- // token pk.eyJ1IjoiZGFqY2V2ZWRvIiwiYSI6ImNrOXlsYWZzNjB3MWEzZHBieWJ1ZmpjbmEifQ.j1xzYdB-O7-otkZEzhsDNg
+ // token pk.eyJ1IjoiZGFqY2V2ZWRvIiwiYSI6ImNrMjM2c3hvejBsdzUzYm94cTcxNDU3OHYifQ.6YRlLDSJO2pHGvkrWRmV8QNg
 class MapPage extends StatelessWidget {
+
+  final mapCtrl = new MapController();
+  final token = "pk.eyJ1IjoiZGFqY2V2ZWRvIiwiYSI6ImNrMjM2c3hvejBsdzUzYm94cTcxNDU3OHYifQ.6YRlLDSJO2pHGvkrWRmV8Q";
   @override
   Widget build(BuildContext context) {
 
@@ -16,7 +21,60 @@ class MapPage extends StatelessWidget {
             )
           ],
         ),
-        body: Center(child: Text('${scan.valor}'),)
+        body: _createFlutterMap(scan),
+        floatingActionButton: _makeActionButton(context),
+    );
+  }
+
+  Widget _createFlutterMap(ScanModel scan) {
+    return  FlutterMap(
+      mapController: mapCtrl,
+      options: MapOptions (
+        center: scan.getLatLtn(),
+        zoom: 15.0
+      ),
+      layers: [
+        _renderMap(),
+        _renderTargets(scan)
+      ]
+    );
+  }
+
+  _renderMap() {
+    return TileLayerOptions(
+      urlTemplate: 'https://api.mapbox.com/v4/'
+      '{id}/{z}/{x}/{y}@2x.png?access_token={accessToken}',
+      additionalOptions: {
+        'accessToken': token,
+        'id': 'mapbox.mapbox-streets-v7'
+      }
+    );
+  }
+
+  _renderTargets(ScanModel scan) {
+    return MarkerLayerOptions(
+      markers: <Marker>[
+        Marker(
+          width: 100.0,
+          height: 100.0,
+          point: scan.getLatLtn(),
+          builder: (BuildContext context) => Container(
+            child: Icon(
+              Icons.location_on,
+              size:70.0,
+              color: Theme.of(context).primaryColor
+              ),
+            )
+        )
+      ]
+    );
+  }
+
+  Widget _makeActionButton(BuildContext context) {
+    return FloatingActionButton(
+      onPressed: (){},
+      child: Icon(Icons.repeat),
+      backgroundColor: Theme.of(context).primaryColor,
     );
   }
 }
